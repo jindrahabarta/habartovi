f13dd9ad932a8c024abd5c89914b05c439962679
lubahabarta <habarta@deepvision.cz>
2025-02-02 15:54:07 +0100

    added endpoint for posts

--- src/_axios/axios.ts
+++ src/_axios/axios.ts
@@ -5,3 +5,9 @@ export const igClient = axios.create({
 	timeout: 3000,
 	headers: { 'Content-Type': 'application/json' },
 })
+
+export const nextClient = axios.create({
+	baseURL: '/api/v1',
+	timeout: 5000,
+	headers: { 'Content-Type': 'application/json' },
+})
---
+++ src/app/api/v1/posts/route.ts
@@ -0,0 +1,21 @@
+import { getPosts } from '@/_graphql/posts/getPosts'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+	try {
+		const page = parseInt(req.nextUrl.searchParams.get('page') || '1', 10)
+		const size = parseInt(req.nextUrl.searchParams.get('size') || '6', 10)
+
+		const data = await getPosts(page, size)
+		if (!data) {
+			return NextResponse.json([], { status: 404 })
+		}
+
+		return NextResponse.json(data, { status: 200 })
+	} catch (e) {
+		return NextResponse.json(
+			{ message: 'Internal server error' },
+			{ status: 500 }
+		)
+	}
+}
