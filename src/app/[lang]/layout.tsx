d0e7a6eff30fe0f177a5774873ec9896a1552034
lubahabarta <lubahabarta@gmail.com>
2025-02-02 21:00:47 +0100

    moved links into layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -7,6 +7,7 @@ import Navigation from '@/_components/Navigation/Navigation'
 import Footer from '@/_components/Footer/Footer'
 import FooterEasterEgg from '@/_components/Footer/FooterEasterEgg'
 import { getNavigation } from '@/_constants/navigation'
+import { useTranslation } from '@/_i18n'
 
 const openSans = Open_Sans({
 	subsets: ['latin'],
@@ -38,6 +39,54 @@ export default async function RootLayout({
 	children: React.ReactNode
 	params: { lang: string }
 }>) {
+	const { t } = await useTranslation(lang, 'navigation')
+
+	const links = [
+		{
+			link: `/`,
+			text: t('home'),
+		},
+		{
+			link: `/ubytovani`,
+			text: t('accomodation'),
+			submenu: [
+				{
+					link: `/ubytovani`,
+					text: t('apartments'),
+				},
+				{
+					link: `/ubytovani/maly-apartman`,
+					text: t('studio'),
+				},
+				{
+					link: `/ubytovani/velky-apartman`,
+					text: t('big_apartment'),
+				},
+				{
+					link: `/ubytovani/rezervace`,
+					text: t('reservation'),
+				},
+			],
+		},
+		{
+			link: `/keramicka-dilna`,
+			text: t('pottery'),
+		},
+		{
+			link: `/vylety-do-okoli`,
+			text: t('places_to_visit'),
+		},
+
+		{
+			link: `/blog`,
+			text: t('blog'),
+		},
+		{
+			link: `/kontakt`,
+			text: t('contact'),
+		},
+	]
+
 	return (
 		<html lang={lang} dir={dir(lang)}>
 			<body
@@ -45,7 +94,7 @@ export default async function RootLayout({
 					(openSans.className, oswald.variable)
 				} flex flex-col items-center min-h-screen`}
 			>
-				<Navigation links={await getNavigation(lang)}></Navigation>
+				<Navigation links={links}></Navigation>
 
 				<main className='flex-1 w-full flex flex-col justify-start'>
 					{children}
