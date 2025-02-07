a36a4de40e99f8d2d2fd0eb9cf6058bd993832d4
lubahabarta <habarta@deepvision.cz>
2025-02-07 22:18:31 +0100

    fixed sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,10 +5,10 @@ import { ArrayElement } from '@/_types/helpers/arrayElement'
 import { getCategories } from '@/_graphql/categories/getCategories'
 import { getPosts } from '@/_graphql/posts/getPosts'
 
-function getAlternatesConfig(url: string) {
+function getAlternatesConfig(url: string, path: string) {
 	let languages: { [key: string]: string } = {}
 	for (let i = 0; i < langs.length; i++) {
-		languages[langs[i]] = `/${langs[i]}${url}`
+		languages[langs[i]] = `${url}/${langs[i]}${path}`
 	}
 	return {
 		languages,
@@ -17,14 +17,15 @@ function getAlternatesConfig(url: string) {
 
 function getSiteConfig(
 	url: string,
+	path: string,
 	priority: number = 10
 ): ArrayElement<MetadataRoute.Sitemap> {
 	return {
-		url,
+		url: `${url}/cs${path}`,
 		lastModified: moment('2025-02-07', 'YYYY-MM-DD').toDate(),
 		changeFrequency: 'monthly',
 		priority,
-		alternates: getAlternatesConfig(url),
+		alternates: getAlternatesConfig(url, path),
 	}
 }
 
@@ -32,28 +33,26 @@ export default async function sitemap() {
 	const BASE_URL = 'https://habartovi.cz'
 
 	const map: MetadataRoute.Sitemap = [
-		getSiteConfig(`${BASE_URL}/`),
+		getSiteConfig(BASE_URL, ''),
 
-		getSiteConfig(`${BASE_URL}/ubytovani`),
-		getSiteConfig(`${BASE_URL}/ubytovani/maly-apartman`),
-		getSiteConfig(`${BASE_URL}/ubytovani/velky-apartman`),
-		getSiteConfig(`${BASE_URL}/ubytovani/rezervace`),
+		getSiteConfig(BASE_URL, '/ubytovani'),
+		getSiteConfig(BASE_URL, '/ubytovani/maly-apartman'),
+		getSiteConfig(BASE_URL, '/ubytovani/velky-apartman'),
+		getSiteConfig(BASE_URL, '/ubytovani/rezervace'),
 
-		getSiteConfig(`${BASE_URL}/keramicka-dilna`),
+		getSiteConfig(BASE_URL, '/keramicka-dilna'),
 
-		getSiteConfig(`${BASE_URL}/vylety-do-okoli`),
+		getSiteConfig(BASE_URL, '/vylety-do-okoli'),
 
-		getSiteConfig(`${BASE_URL}/blog`),
+		getSiteConfig(BASE_URL, '/blog'),
 
-		getSiteConfig(`${BASE_URL}/kontakt`),
+		getSiteConfig(BASE_URL, '/kontakt'),
 	]
 
 	const posts = await getPosts(1, 100)
 	if (posts) {
 		for (let i = 0; i < posts.nodes.length; i++) {
-			map.push(
-				getSiteConfig(`${BASE_URL}/blog/${posts.nodes[i].slug}`, 8)
-			)
+			map.push(getSiteConfig(BASE_URL, `/blog/${posts.nodes[i].slug}`, 8))
 		}
 	}
 
@@ -62,7 +61,8 @@ export default async function sitemap() {
 		for (let i = 0; i < categories.nodes.length; i++) {
 			map.push(
 				getSiteConfig(
-					`${BASE_URL}/blog/rubriky/${categories.nodes[i].slug}`,
+					BASE_URL,
+					`/blog/rubriky/${categories.nodes[i].slug}`,
 					6
 				)
 			)
